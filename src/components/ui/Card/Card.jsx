b25a239a57fb84a94345b91ec183df008ae58874
idpan <idpanashari813@gmail.com>
2023-08-15 01:12:06 +0700

    fix : add animation and fix layout

--- src/components/ui/Card/Card.jsx
+++ src/components/ui/Card/Card.jsx
@@ -1,36 +1,17 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import WatchlistBtn from "../WatchlistBtn/WatchlistBtn";
 import WatchNowBtn from "../WatchNowBtn/WatchNowBtn";
 import style from "./card.module.css";
 import "./cardHover.css";
 const image_base = import.meta.env.VITE_BASE_IMAGE_URL;
 function Card(props) {
-  const cardRef = useRef(null);
-  const previewRef = useRef(null);
-  const thumbRef = useRef(null);
-  let onHover; //to store setTimeOut
   return (
-    <div
-      ref={cardRef}
-      onMouseEnter={(e) => {
-        onHover = setTimeout(() => {
-          // previewRef.current.style.display = "block";
-          // thumbRef.current.style.display = "none";
-          cardRef.current.classList.add("onHover");
-        }, 500);
-      }}
-      onMouseLeave={(e) => {
-        clearTimeout(onHover);
-        // previewRef.current.style.display = "none";
-        // thumbRef.current.style.display = "block";
-        cardRef.current.classList.remove("onHover");
-      }}
-      className={style.card}
-    >
-      <div ref={thumbRef} className={`${style.card_thumbnail} card_thumbnail`}>
+    <div className={style.card}>
+      <div className={`${style.card_thumbnail} card_thumbnail`}>
         <img src={image_base + props.poster} alt={props.title} />
       </div>
-      <div ref={previewRef} className={`${style.card_preview} card_preview `}>
+
+      <div className={`${style.card_preview} card_preview `}>
         <div className={style.preview_thumbnail}>
           <img src={image_base + props.backdrop_path} alt={props.title} />
           <div className={style.layer}></div>
@@ -39,8 +20,6 @@ function Card(props) {
           <div className={style.card_btn_container}>
             <WatchNowBtn />
             <WatchlistBtn />
-            {/* <button className={style.watchNow_btn}>watch Now</button>
-          <button className={style.addToWatchList_btn}>+</button> */}
           </div>
           <div className={style.info}>
             <span>{props.release_year}</span>
